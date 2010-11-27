3ef71b952cedbe215abfbb3fe10fa45c1870f94a
Ben Myers <ben@ben-myers.com>
2010-11-27 20:21:05 +0530

    About to do CSS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'e99f0ed29e409f89de22643bac8e58f50d48889155e13f23332ce800dbf940a9f1de6b28077dadb28b2947c6bc41496ca67473e9ef86d6f4ff145478a2e672b6'
